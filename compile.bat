9cc0aa185d1e9aa79a55095e806e676dc14003cf
talbii <talbioffical@gmail.com>
2021-08-31 02:55:04 +0300

    Added compile.bat for Windows

---
+++ compile.bat
@@ -0,0 +1,27 @@
+echo off
+
+:: NOTE: This doesn't deal with additional arguments . . . yet
+
+IF %1.==. GOTO NoSourceFile
+
+setlocal EnableExtensions
+
+set SourceFile=%1.c
+set CurrDirectory_ofile="%~dp0%1.exe"
+
+:: Gets a unique file name (temporary file):
+:uniqLoop
+set "uniqueFileName=%tmp%\bat~%RANDOM%.c"
+if exist "%uniqueFileName%" goto :uniqLoop
+
+:: for some reason, code with strings (Beginstr/Endstr) only work if I run the preprocessor seperately.
+gcc -E %SourceFile% > %uniqueFileName%
+
+gcc %uniqueFileName% -o %CurrDirectory_ofile% -x c
+
+exit /b 0
+
+:NoSourceFile
+    echo ^[91mfatal error:[0m no input files
+    echo compilation terminated.
+    exit /b 1
