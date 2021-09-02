5e1ad07a6bc708cbfe7e19a63ee3e99ddff887d6
talbii <talbioffical@gmail.com>
2021-09-02 15:57:52 +0300

    Added numless Package

---
+++ packages/vbpl_numless.h
@@ -0,0 +1,15 @@
+#ifndef _VBPL_P_numless_H
+#define _VBPL_P_numless_H
+
+#define Zero  0
+#define One   1
+#define Two   2
+#define Three 3
+#define Four  4
+#define Five  5
+#define Six   6
+#define Seven 7
+#define Eight 8
+#define Nine  9
+
+#endif // _VBPL_P_numless_H
